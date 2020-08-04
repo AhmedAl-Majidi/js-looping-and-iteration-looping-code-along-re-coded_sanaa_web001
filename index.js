f2754fb8b6cdc59712fd599a5e8d8bc15ef1059c
Ahmed Al-Majidi <af774857999@gmail.com>
2020-08-04 16:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(names, eventName) {
   for (var i = 0; i < names.length; i++) {
-    console.log(`Thank you, Ada, ${names[i]}, for the wonderful ${eventName} gift!`);
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
   }
 }
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
